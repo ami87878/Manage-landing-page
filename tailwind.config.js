b0259162aee79c6a8ba3d00dddd3f05b126f9ab3
Amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2023-09-20 23:16:12 +0330

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{
+
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+
+
+    },
+    extend: {
+
+
+      colors:{
+        brightRed:'hsl(12,88%,59%)',
+        brightRedLight:'hsl(12,88%,69%)',
+        brightRedSupLight:'hsl(12,88%,95%)',
+        darkBlu:'hsl(228,39%,23%)',
+        darkGrayishBlu:'hsl(227,12%,61%)',
+        verydarkBlu:'hsl(223,12%,13%)',
+        veryPaleRed:'hsl(13,100%,96%)',
+        veryLightGray:'hsl(0,0%,98%)',
+
+
+      }
+    },
   },
   plugins: [],
 }
